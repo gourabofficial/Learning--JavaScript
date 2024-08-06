c3255d569c6f9e28b3999045d3811e774e97d1c3
gourabofficial <gangulygourab3@gmail.com>
2024-08-06 12:37:14 +0530

    explore Data type

---
+++ Basic/datatype.js
@@ -0,0 +1,34 @@
+const Myid = Symbol("132");
+const id = Symbol("132");
+
+// console.log(Myid === id);
+// console.log(Myid)
+// console.log(id)
+
+
+let  name = "ishowspeed"
+age = 21;
+country = "USA"
+isStreemer = true
+let bestYoutuber
+let mobileNum = null
+
+ 
+
+function hello () {
+    console.log("Hello world")
+    
+}
+hello()
+console.log(typeof hello)
+console.log(typeof age)
+console.log(typeof name)
+console.log(typeof country)
+console.log(typeof isStreemer)
+console.log(typeof bestYoutuber)
+console.log("Null type: " + typeof mobileNum)
+
+
+
+
+
