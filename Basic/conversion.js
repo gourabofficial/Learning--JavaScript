7a901c4bf1723d6f71c163db604c0895c4c22178
gourabofficial <gangulygourab3@gmail.com>
2024-08-06 12:01:42 +0530

    basic operation

---
+++ Basic/conversion.js
@@ -0,0 +1,13 @@
+let num = "7"
+let name = "gourab"
+
+let changeName = Number(name)
+console.log(changeName);
+console.log(typeof changeName);
+
+
+
+// let changeNum = String(num)
+// console.log(changeNum);
+
+// console.log(typeof num);
