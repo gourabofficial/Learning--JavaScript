46d77d70bb29fc270d8706c74ad2580e423092d8
gourabofficial <gangulygourab3@gmail.com>
2024-08-07 20:37:32 +0530

    stack vs heap memory

---
+++ Basic/memory.js
@@ -0,0 +1,12 @@
+let userOne = {
+    email: "userone@google.co.in",
+    name : "sourav"
+}
+
+let userTwo = userOne;
+userTwo.name = "Gourab"
+
+
+console.log(userOne.name)
+console.log(userTwo.name)
+
