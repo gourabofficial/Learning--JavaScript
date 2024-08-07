d128068f66d073bf5d899eabb332c04462b73cdc
gourabofficial <gangulygourab3@gmail.com>
2024-08-07 23:14:22 +0530

    string explore

---
+++ Basic/string.js
@@ -0,0 +1,21 @@
+const mystring = "gourab"
+const mystring2 = " now i am doing JavaScript"
+console.log(`Hello my name is ${mystring} ${mystring2}`) // the best way to string concatination
+
+
+const myname = new String("Gourab07")
+console.log(myname)
+console.log(myname.charAt(3))
+console.log(myname.indexOf(7))
+//console.log(myname.substring(4))
+console.log(myname.slice(4))
+
+
+const secondName = "    gourabganguly  "
+console.log("After : " + secondName)
+console.log("Before: " + secondName.trim())
+
+
+
+const url = "https://gourab.com/25$gourabofficial.in"
+console.log(url.replace("25$", "-"))
