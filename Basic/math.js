d9d9c5f73dd8ab4901db0cb24834150f79afbd4b
gourabofficial <gangulygourab3@gmail.com>
2024-08-08 19:23:41 +0530

    Math&Num

---
+++ Basic/math.js
@@ -0,0 +1,15 @@
+// console.log(Math);
+// console.log(Math.abs(-4)); // nagative value convart to positive 
+// console.log(Math.round(4.55)); // .55 remove ,ans is : only 4
+// console.log(Math.ceil(4.1)); // if 4.1 = 5, 7.5 = 8
+// console.log(Math.floor(4.7789789));// if 5.8 = 5,  
+// console.log(Math.sqrt(16)); // 16 = 4*4;
+// console.log(Math.min(2, 3, 4, 56, 7, 8, 9,8)); // min value 
+// console.log(Math.max(9,8,7,65,4,2)); // max value
+
+console.log(Math.floor((Math.random(2) * 10) + 1));
+
+const min = 10
+const max = 20
+
+console.log(Math.floor(Math.random() * (min - max + 1) )+ min );
---
+++ Basic/num.js
@@ -0,0 +1,15 @@
+const score = 22
+console.log(score)
+
+const balance = new Number(100)
+console.log(balance)
+
+console.log(balance.toFixed(2))
+
+const newNum = new Number(15.5647)
+console.log(newNum.toPrecision(3))
+
+
+const thousand = 1000000
+console.log(thousand.toLocaleString('en-IN'));
+
