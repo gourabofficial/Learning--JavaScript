4a19a71114ab1b8581736a398eaae1bc3976e69f
gourabofficial <gangulygourab3@gmail.com>
2024-08-09 22:37:24 +0530

    object..

--- Object/second.js
+++ Object/second.js
@@ -1,8 +1,8 @@
-const obj1 = { 1: "gourab", 2: "ronaldo" }
+const obj1 = { 1: "gourab", 2: "ronaldo", }
 const obj2 = { 3: "cristano", 4: "messi" }
 
 const obj3 = { ...obj1, ...obj2 }
-// console.log(obj3)
+console.log(obj3)
 
 
 
@@ -19,3 +19,9 @@ const regularUser = {
 }
 console.log(regularUser)
 console.log(regularUser.fullName.username);
+
+
+console.log(Object.keys(regularUser)) // some functions (keys)
+console.log(Object.values(regularUser)) // showing the values 
+console.log(regularUser.hasOwnProperty('study')) // exits or not (study)
+console.log(regularUser.hasOwnProperty('hobby'))
